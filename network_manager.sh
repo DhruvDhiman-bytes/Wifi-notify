69c9201379ef98851e1b2eafe952fe0a55bbdea9
Dhruv Dhiman <dhruvdhiman905@gmail.com>
2026-06-19 00:33:37 +0530

    Added the feature to the file of printing the speed of the wifi

--- network_manager.sh
+++ network_manager.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-nmcli -t -f SSID,SIGNAL,BARS dev wifi
+nmcli -t -f SSID,SIGNAL,BARS,RATE dev wifi
 # run nmcli and store output into array splitting on ':'
-IFS=':' read -r -a INFO < <(nmcli -t -f SSID,SIGNAL,BARS dev wifi | head -1)
+IFS=':' read -r -a INFO < <(nmcli -t -f SSID,SIGNAL,BARS,RATE dev wifi | head -1)
 
 # print the full array
 echo "Full array: ${INFO[@]}"
@@ -14,6 +14,7 @@ echo "Total elements: ${#INFO[@]}"
 SSID="${INFO[0]}"
 SIGNAL="${INFO[1]}"
 BARS="${INFO[2]}"
+SPEED="${INFO[3]}"
 
 echo
 echo "Ran the script perfectly fine"
